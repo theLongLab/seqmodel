MINOR/FIX conv output length calculation

src/seqmodel/model/conv.py
@@ -10,22 +10,26 @@ class CellModule(nn.Module):
         self.activation_fn = activation_fn
         self.layers = nn.Sequential(*layer_list)
 
-    # number of channels per sequence position
-    @property
-    def out_channels(self):
-        self.layers
-
     def forward(self, x):
         return self.layers(x)
+
+    # gets the output number of channels per sequence position
+    @property
+    def out_channels(self):
+        for module in reversed(list(self.modules())):
+            if hasattr(module, 'out_channels'):
+                channels = module.out_channels
+                break
+        return channels
 
     def out_seq_len(self, seq_len):
         for module in self.modules():
             if type(module) == nn.Conv1d:
-                #FIXME: this calculation is wrong
                 seq_len += 2 * module.padding[0] + module.stride[0]
-                seq_len -= module.kernel_size[0] * module.dilation[0]
+                seq_len -= module.kernel_size[0] + (module.kernel_size[0] - 1) * (module.dilation[0] - 1)
                 seq_len = seq_len // module.stride[0]
         return seq_len
+
 
 class SeqFeedForward(CellModule):
     """

test/model/test_conv.py
@@ -17,20 +17,25 @@ class Test_Conv(unittest.TestCase):
 
     def test_CellModule(self):
         conv_list = [
-            nn.Conv1d(4, 10, 3),
-            nn.Conv1d(4, 10, 3, padding=5),
-            nn.Conv1d(4, 10, 5, stride=3),
-            nn.Conv1d(4, 10, 4, dilation=2),
+            nn.Conv1d(4, 4, 3),
+            nn.Conv1d(4, 4, 3, padding=5),
+            nn.Conv1d(4, 4, 5, stride=3),
+            nn.Conv1d(4, 4, 4, dilation=2),
             nn.Conv1d(4, 10, 4, padding=3, stride=2, dilation=2),
         ]
         for conv in conv_list:
             layer = CellModule([conv])
-            self.assertEqual(layer.out_seq_len(self.length), conv(self.x).shape[2])
+            tensor = conv(self.x)
+            self.assertEqual(layer.out_channels, tensor.shape[1])
+            self.assertEqual(layer.out_seq_len(self.length), tensor.shape[2])
+            npt.assert_array_equal(layer(self.x).detach(), tensor.detach())
         layer = CellModule(conv_list)
         tensor = self.x
         for conv in conv_list:
             tensor = conv(tensor)
-        self.assertEqual(layer.out_seq_len(self.length), conv(self.x).shape[2])
+        self.assertEqual(layer.out_channels, tensor.shape[1])
+        self.assertEqual(layer.out_seq_len(self.length), tensor.shape[2])
+        npt.assert_array_equal(layer(self.x).detach(), tensor.detach())
 
     def test_SeqFeedForward(self):
         layer = SeqFeedForward(self.in_channels, self.out_channels)
@@ -54,6 +59,10 @@ class Test_Conv(unittest.TestCase):
         out = layer(self.x).detach()
         #TODO: finish
         # self.assertEqual(out.shape, (self.batches, self.out_channels, self.length))
+
+    def test_DilateConvEncoder(self):
+        pass
+        #TODO: finish
 
 
 if __name__ == '__main__':