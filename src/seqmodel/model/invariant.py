93ed09ccc30c154ea956034fb923e6fc63c84174
Devin Kwok <devin@devinkwok.com>
2020-07-31 16:05:45 -0600

    ADD/FIX invariant convolution layer unit tests.
    ADD unit tests for `model/invariant.py`
    FIX order of output channels so that they can also be complemented.

--- src/seqmodel/model/invariant.py
+++ src/seqmodel/model/invariant.py
@@ -11,35 +11,26 @@ class RCIConv1d(nn.Conv1d):
                 stride=1, padding=0, dilation=1, groups=1, bias=True,
                 do_reverse=True, do_complement=True):
 
-        transforms = [super().forward]
-        divisor = 1
-        if do_reverse:
-            transforms += [self.reverse]
-        if do_complement:
-            divisor = 2
-            transforms += [self.complement]
+        transforms = [nn.Identity()]
         if do_reverse and do_complement:
-            divisor = 4
-            transforms += [self.reverse_complement]
-        assert out_channels % divisor == 0
+            transforms += [reverse_complement]
+        if do_reverse:
+            transforms += [reverse]
+        if do_complement:
+            transforms += [complement]
+        assert out_channels % len(transforms) == 0
 
-        super().__init__(in_channels, out_channels // divisor, kernel_size,
+        super().__init__(in_channels, out_channels // len(transforms), kernel_size,
                 stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias)
         self._transforms = transforms
         self.out_channels = out_channels
 
-    def complement(self, x):
-        return self(complement(x))
-
-    def reverse(self, x):
-        return reverse(self(reverse(x)))
-
-    def reverse_complement(self, x):
-        return reverse(self(reverse_complement(x)))
-
+    # output channels are ordered as (normal, reverse_complement, reverse, complement)
+    # complementation is still meaningful by flipping (reversing) the channel dimension
     def forward(self, x):
-        return torch.cat([t(x) for t in self._transforms], dim=1)
-
+        return torch.cat([t(super(RCIConv1d, self).forward(t(x)))
+                        for t in self._transforms], dim=1)
+    
 
 # stack outputs of arbitrary modules along dimension
 class CombineChannels(nn.Module):
--- test/model/test_invariant.py
+++ test/model/test_invariant.py
@@ -5,32 +5,53 @@ import torch.nn as nn
 import numpy.testing as npt
 
 from seqmodel.model.invariant import *
+from seqmodel.seq.transform import complement, reverse, reverse_complement, one_hot
+from seqmodel.seq.mapseq import create_test_batch
 
 
 class Test_Invariant(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.batch_size, self.seq_len = 5, 37
+        self.x = one_hot(create_test_batch(self.batch_size, self.seq_len))
 
     def test_RCIConv1d(self):
-        in_channels, out_channels, kernel_size = 4, 8, 3
-        batch_size, seq_len = 5, 7
-
+        in_channels, out_channels, kernel_size = 4, 8, 7
         rci_conv = RCIConv1d(in_channels, out_channels, kernel_size,
-                do_reverse=False, do_complement=False)
-        conv = nn.Conv1d(in_channels, out_channels, kernel_size)
+                do_reverse=False, do_complement=False, padding=kernel_size // 2)
+        conv = nn.Conv1d(in_channels, out_channels, kernel_size, padding=kernel_size // 2)
         conv.weight = rci_conv.weight
         conv.bias = rci_conv.bias
-
-        x = torch.randn(batch_size, in_channels, seq_len)
-        self.assertEqual(rci_conv(x).shape, conv(x).shape)
-        npt.assert_array_equal(rci_conv(x).detach(), conv(x).detach())
+        self.assertEqual(rci_conv(self.x).shape, (self.batch_size, out_channels, self.seq_len))
+        npt.assert_array_equal(rci_conv(self.x).detach(), conv(self.x).detach())
 
         rci_conv = RCIConv1d(in_channels, out_channels, kernel_size,
                 do_reverse=True, do_complement=False)
+        y1 = reverse(rci_conv(reverse(self.x))).detach()
+        y2 = rci_conv(self.x).detach()
+        # have to compare different halves of the array because the
+        # normal and reversed output channels have switched places
+        # assume output channels are grouped as (normal, reverse_complement, reverse, complement)
+        # need to compare to (reverse, complement, normal, reverse_complement)
+        npt.assert_array_equal(y1[:, :4, :], y2[:, 4:8, :])
+        npt.assert_array_equal(y1[:, 4:8, :], y2[:, :4, :])
 
         rci_conv = RCIConv1d(in_channels, out_channels, kernel_size,
                 do_reverse=False, do_complement=True)
+        y1 = complement(rci_conv(complement(self.x))).detach()
+        y2 = rci_conv(self.x).detach()
+        # don't need to switch because complementation is done along the channel dimension
+        # this means every individual channel's position is mirrored relative to its complement
+        # i.e. channel 0 is the complement of channel -1, 1 is the complement of -2, etc.
+        npt.assert_array_equal(y1, y2)
+
+        rci_conv = RCIConv1d(in_channels, out_channels, kernel_size,
+                do_reverse=True, do_complement=True)
+        y1 = reverse_complement(rci_conv(reverse_complement(self.x))).detach()
+        y2 = rci_conv(self.x).detach()
+        # same issue as above re. reversing output channels
+        npt.assert_array_equal(y1[:, :4, :], y2[:, 4:8, :])
+        npt.assert_array_equal(y1[:, 4:8, :], y2[:, :4, :])
 
 
 if __name__ == '__main__':
