ADD/MODIFY/FIX `seqbert.py` cls token, masking
ADD classification token for next sequence prediction pretext task.
This uses transforms to split and permute sequences,
and adds separate `cls_regularization` loss based on
prediction of whether each sequence in the batch was permuted.

MODIFY sequence masking logic for BERT model moved from `mask.py` to
`data_transform()` and `mask_transform()` in `seqbert.py`.
This concentrates the model-specific logic in one location.

FIX remove attention masking in `torch.nn.TransformerEncoder`,
this was incorrectly removing information for masked position prediction.
Instead, only need to replace tokens in the source sequence with a
mask token.

REMOVE flanking sequences in `mask.py`, this is no longer necessary
as it is easier to control flanking sequences in the data preprocessing
steps of the model itself.

src/experiment/seqbert.py
@@ -11,12 +11,13 @@ from pytorch_lightning import Trainer, seed_everything
 
 from seqmodel.model.conv import DilateConvEncoder, SeqFeedForward
 from seqmodel.model.attention import SinusoidalPosition
-from seqmodel.functional.mask import PositionMask
+from seqmodel.functional.mask import generate_mask, mask_randomize, mask_fill, mask_select
 from seqmodel.seqdata.mapseq import RandomRepeatSequence
 from seqmodel.seqdata.iterseq import StridedSequence, bed_from_file, FastaFile
-from seqmodel.functional.transform import INDEX_TO_BASE, Compose, one_hot_to_index
+from seqmodel.functional.transform import INDEX_TO_BASE, Compose, bioseq_to_index, \
+                            single_split, permute
 from seqmodel.functional.log import prediction_histograms, normalize_histogram, \
-                            summarize, correct, accuracy_per_class
+                            summarize, correct, accuracy_per_class, accuracy
 
 
 # from https://github.com/PyTorchLightning/pytorch-lightning/issues/2534
@@ -60,26 +61,47 @@ class CheckpointEveryNSteps(pl.Callback):
 
 class SeqBERT(LightningModule):
 
+    TOKENS_BP = INDEX_TO_BASE + [  # AGCT 0 1 2 3
+        'n',  # 4 unknown base
+        'm',  # 5 masked base
+        '~',  # 6 classification token (always at start)
+        'f',  # 7 output token at classification token position, indicates pretext task false
+        't',  # 8 output token indicating pretext task is true
+        ]
+    MASK_TOKEN = 5
+    CLS_TOKEN = 6
+    CLS_OFFSET = 7
+    # for mask
+    NO_LOSS_INDEX = 0
+    MASK_INDEX = 1
+    RANDOM_INDEX = 2
+    KEEP_INDEX = 3
+
     def __init__(self, **hparams):
         super().__init__()
         self.save_hyperparameters()
+
+        self.tokens = self.TOKENS_BP
+        embedding = nn.Embedding(len(self.tokens), self.hparams.n_dims)
         if self.hparams.position_embedding == 'Sinusoidal':
             self.embedding = nn.Sequential(
-                nn.Embedding(4, self.hparams.n_dims),
+                embedding,
                 SinusoidalPosition(self.hparams.n_dims, dropout=self.hparams.dropout,
-                                    max_len=self.hparams.seq_len),
+                                    max_len=(self.hparams.seq_len + 1)),  # add 1 for cls token
                 )
         else:
-            self.embedding = nn.Embedding(4, self.hparams.n_dims)
+            self.embedding = embedding
+
         self.transformer = nn.TransformerEncoder(
                 nn.TransformerEncoderLayer(self.hparams.n_dims, self.hparams.n_heads,
                                     self.hparams.feedforward_dims, self.hparams.dropout),
                 self.hparams.n_layers)
-        self.decoder = SeqFeedForward(self.hparams.n_dims, 4, hidden_layers=self.hparams.n_decode_layers - 1,
-                                activation_fn=nn.ReLU)
+
+        self.decoder = SeqFeedForward(self.hparams.n_dims, len(self.tokens),
+                        hidden_layers=self.hparams.n_decode_layers - 1, activation_fn=nn.ReLU)
+
         self.loss_fn = nn.CrossEntropyLoss()
-        self.mask = PositionMask(mask_prop=self.hparams.mask_prop, random_prop=self.hparams.random_prop,
-                                    keep_prop=self.hparams.keep_prop,)
+        self.mask_props = (self.hparams.mask_prop, self.hparams.random_prop, self.hparams.keep_prop)
 
     def configure_optimizers(self):
         return torch.optim.Adam(chain(self.embedding.parameters(), self.transformer.parameters(),
@@ -96,7 +118,8 @@ class SeqBERT(LightningModule):
             if self.hparams.train_intervals is not None:
                 intervals = bed_from_file(self.hparams.train_intervals)
             train_data = StridedSequence(FastaFile(self.hparams.seq_file),
-                        self.hparams.seq_len, include_intervals=intervals, sequential=False)
+                        self.hparams.seq_len, include_intervals=intervals,
+                        transforms=bioseq_to_index, sequential=False)
         return train_data.get_data_loader(self.hparams.batch_size, self.hparams.num_workers)
 
     def val_dataloader(self):
@@ -110,11 +133,49 @@ class SeqBERT(LightningModule):
             if self.hparams.valid_intervals is not None:
                 intervals = bed_from_file(self.hparams.valid_intervals)
             valid_data = StridedSequence(FastaFile(self.hparams.seq_file),
-                        self.hparams.seq_len, include_intervals=intervals, sequential=True)
+                        self.hparams.seq_len, include_intervals=intervals, 
+                        transforms=bioseq_to_index, sequential=True)
         return valid_data.get_data_loader(self.hparams.batch_size, self.hparams.num_workers)
 
+    def data_transform(self, x_batch):
+        with torch.no_grad():
+            # split into 2 halves, permute half of the halves
+            first, last = single_split(x_batch)
+            is_permuted, last = permute(last)
+            # recombine with classification boolean at front, set to correct token indexes by adding offset
+            cls_target = is_permuted.to(x_batch.dtype) + self.CLS_OFFSET
+            return torch.cat((cls_target.unsqueeze(1), first, last), dim=1)
+
+    def mask_transform(self, target):
+        with torch.no_grad():
+            # randomize, mask, or mark for loss calculation some proportion of positions
+            mask = generate_mask(target, self.mask_props)
+            # omit classification token (1st position in seq) from masked prediction loss
+            mask[:, 0] = self.NO_LOSS_INDEX
+            source = mask_randomize(target, mask == self.RANDOM_INDEX, 4)  # number of random tokens = base pairs
+            source = mask_fill(source, mask == self.MASK_INDEX, self.MASK_TOKEN)
+            # make the first position the classification token
+            source[:, 0] = self.CLS_TOKEN
+            # return mask of all positions that will contribute to loss
+            return source, target, mask
+
+    def masked_forward(self, x):
+        target = self.data_transform(x)
+        source, target, mask = self.mask_transform(target)
+        # input dims are (batch, seq), embedding adds channel dim to end
+        # swap dimensions from (batch, seq, channel) to (seq, batch, channel)
+        latent = self.transformer(self.embedding(source).permute(1, 0, 2))
+        # swap dimensions from (seq, batch, channel) to (batch, channels, seq_len)
+        predicted = self.decoder(latent.permute(1, 2, 0))
+        masked_predict_loss = self.loss_fn(mask_select(predicted, mask != self.NO_LOSS_INDEX),
+                                            mask_select(target, mask != self.NO_LOSS_INDEX))
+        # apply classification loss separately
+        cls_loss = self.loss_fn(predicted[:, 0,:], target[:, 0])
+        loss = masked_predict_loss + self.hparams.cls_regularization * cls_loss
+        return loss, predicted, latent, source, target, mask
+
     def forward(self, batch):
-        x, seqname, coord = batch
+        x, (seqname, coord) = batch
         # swap dimensions from (batch, seq, channel) to (seq, batch, channel)
         x_1 = self.embedding(x).permute(1, 0, 2)
         z = self.transformer(x_1)
@@ -122,38 +183,36 @@ class SeqBERT(LightningModule):
         y = self.decoder(z.permute(1, 2, 0))
         return y
 
-    def masked_forward(self, x_in):
-        # swap dimensions from (batch, seq, channel) to (seq, batch, channel)
-        x, mask = self.mask.attn_mask(x_in, mask_value=True, mask_fill=True)
-        latent = self.transformer(self.embedding(x).permute(1, 0, 2), src_key_padding_mask=mask)
-        # swap dimensions from (seq, batch, channel) to (batch, channels, seq_len)
-        predicted = self.decoder(latent.permute(1, 2, 0))
-        loss = self.loss_fn(*self.mask.select(predicted, x_in))
-        return loss, predicted, latent, x
-
     def training_step(self, batch, batch_idx):
-        x_in, seqname, coord = batch
-        loss, predicted, _, x = self.masked_forward(x_in)
+        x, (seqname, coord) = batch
+        loss, predicted, latent, source, target, mask = self.masked_forward(x)
         if batch_idx % self.hparams.print_progress_freq == 0:
-            self.print_progress(predicted, x_in, x)
+            self.print_progress(predicted, latent, source, target, mask, seqname, coord)
         return {'loss': loss, #'seqname': seqname[-1], 'coord': coord[-1],
                 'log': {'train_loss': loss,} #'seqname': seqname[-1], 'coord': coord[-1],},
                 }
 
-    def print_progress(self, predicted, x_in, x):
-        str_train_sample = summarize(self.mask.mask_val + 4, x, correct(predicted, x_in),
-                predicted.permute(1, 0, 2), index_symbols=INDEX_TO_BASE + [' ', '_', '?', '='])
-        hist = prediction_histograms(predicted.detach().cpu(), x_in.detach().cpu(), n_bins=5)
+    def print_progress(self, predicted, latent, source, target, mask, seqname, coord):
+        str_train_sample = summarize(
+            mask + len(self.tokens),
+            source,
+            target,
+            correct(predicted, target),
+            predicted.permute(1, 0, 2),
+            index_symbols=self.tokens + [' ', '_', '?', '='])
+        hist = prediction_histograms(predicted.detach().cpu(), target.detach().cpu(), n_bins=5)
         acc = normalize_histogram(hist)
-        acc_numbers = accuracy_per_class(hist, threshold_prob=0.5)
+        acc_numbers = accuracy_per_class(hist, threshold_prob=1. / len(self.tokens))
         str_acc = summarize(acc, col_labels=INDEX_TO_BASE, normalize_fn=None)
-        print(acc_numbers, str_acc, str_train_sample, sep='\n')
+        cls_acc = accuracy(predicted[:, 0], target[:, 0])
+        print(seqname[0], coord[0], cls_acc, acc_numbers)
+        print(str_acc, str_train_sample, sep='\n')
 
     def validation_step(self, batch, batch_idx):
-        x_in, seqname, coord = batch
-        loss, predicted, _, x = self.masked_forward(x_in)
+        x, (seqname, coord) = batch
+        loss, predicted, latent, source, target, mask = self.masked_forward(x)
         if batch_idx % self.hparams.print_progress_freq == 0:
-            self.print_progress(predicted, x_in, x)
+            self.print_progress(predicted, latent, source, target, mask, seqname, coord)
         return {'loss': loss,
                 'log': {
                     'val_loss': loss,
@@ -178,6 +237,7 @@ class SeqBERT(LightningModule):
         parser.add_argument('--n_decode_layers', default=1, type=int)
         parser.add_argument('--feedforward_dims', default=512, type=int)
         parser.add_argument('--dropout', default=0.1, type=float)
+        parser.add_argument('--cls_regularization', default=1., type=float)
         parser.add_argument('--position_embedding', default='Sinusoidal', type=str)
 
         # training params