2312b9a3aea3bd16c8fdd83c5d928eb028059d1e
Devin Kwok <devin@devinkwok.com>
2021-01-04 13:28:38 -0700

    MINOR/FIX training scripts, hparams

--- seqbert-cedar.sh
+++ seqbert-cedar.sh
@@ -3,7 +3,7 @@
 #SBATCH --account=def-quanlong            # needed for resource billing if using compute canada
 #SBATCH --time=12:00:00                           # max walltime in D-HH:MM or HH:MM:SS
 #SBATCH --cpus-per-task=4             # number of cores
-#SBATCH --gres=gpu:p100:1                 # type and number of GPU(s) per node
+#SBATCH --gres=gpu:v100l:1                 # type and number of GPU(s) per node
 #SBATCH --mem=16000                                       # max memory (default unit is MB) per node
 #SBATCH --output=seqbert-bp-%j.out                 # file name for the output
 #SBATCH --error=seqbert-bp-%j.err                  # file name for errors
@@ -53,31 +53,34 @@ mkdir $OUT_DIR
 
 # hparams
 python $SOURCE_DIR/src/exp/seqbert/pretrain.py \
-    --deterministic=True \
     --n_dims=512 \
     --n_heads=4 \
     --n_layers=4 \
     --n_decode_layers=2 \
     --feedforward_dims=1024 \
     --position_embedding=Sinusoidal \
-    --batch_size=32 \
-    --learning_rate=1e-4 \
-    --seq_len=1000 \
-    --dropout=0.0 \
-    --keep_prop=0.03 \
-    --mask_prop=0.1 \
+    --batch_size=40 \
+    --learning_rate=3e-4 \
+    --seq_len=2000 \
+    --dropout=0. \
+    --keep_prop=0.01 \
+    --mask_prop=0.07 \
     --random_prop=0.02 \
     --cls_regularization=0.01 \
-    --num_workers=4 \
+    --seq_len_source_multiplier=2. \
+    --crop_factor=0.2 \
+    --seq_len_sample_freq=0.25 \
+    --num_workers=8 \
     --print_progress_freq=500 \
     --save_checkpoint_freq=5000 \
     --seq_file=$RUN_DIR/$NAME_DIR/data/ref_genome/p12/assembled_chr/GRCh38_p12_assembled_chr.fa \
     --train_intervals=$RUN_DIR/$NAME_DIR/data/ref_genome/grch38-train.bed \
     --valid_intervals=$RUN_DIR/$NAME_DIR/data/ref_genome/grch38-1M-valid.bed \
     --default_root_dir=$OUT_DIR \
-    --load_checkpoint_path=$OUT_DIR/lightning_logs/version_55300997/checkpoints/N-Step-Checkpoint_0_170000.ckpt \
 
+    # --load_checkpoint_path=$OUT_DIR/lightning_logs/version_55300997/checkpoints/N-Step-Checkpoint_0_170000.ckpt \
     # --accumulate_grad_batches=1 \
+    # --deterministic=True \
 
 ## clean up by stopping virtualenv
 deactivate
